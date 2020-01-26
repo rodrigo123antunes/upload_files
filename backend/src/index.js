6299e021098bd237fd5d61caf1ca649880e9166b
Rodrigo_bpz <rodrigobpzantunes2016@gmail.com>
2020-01-26 01:05:06 -0200

    Frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const cors = require('cors');
 const path = require("path");
 
 const app = express();
@@ -8,11 +9,12 @@ const app = express();
 /**
  * Database setup
  */
-mongoose.connect('mongodb+srv://rodrigo:ckdr35251665@cluster0-sn65m.mongodb.net/test', {
+mongoose.connect('mongodb+srv://<user>:<password>@cluster0-sn65m.mongodb.net/test', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
